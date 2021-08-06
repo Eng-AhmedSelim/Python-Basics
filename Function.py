ae30be63347c10d655bda084e67e04e6658c421b
Eng-AhmedSelim <dream2015aa@gmail.com>
2021-08-06 19:46:02 +0300

    Don All function file

--- Function.py
+++ Function.py
@@ -56,5 +56,44 @@ print(w)
 print("======================================")
 
 #===================================
-# Example 5
+# Example 5 enumerate function
 #===================================
+name = ["Ahmed","yaser","Ali", "Mohamed","yaseen", "nabil"]
+
+for count , valuo in enumerate(name): # loop on count and valuo in list
+    print(count)
+    print(valuo)
+    if count == 1: # When you get to the count, print out.
+        print('in 1--->')
+    elif count == 3:
+        print('count in ---3')
+#===================================
+# Example 6 map function
+#===================================
+v = [1,2,3,4,5,6,7]
+
+def power(n):
+    return n*n
+
+b = map(power , v)
+print(list(b))
+#===================================
+# Example 7 zip function
+#===================================
+names = ['mahmoud','Ahmed','ali']
+degre = [47,90,33]
+students = zip(names,degre)
+print(list(students))
+
+#===================================
+# Example 8 filter function
+#===================================
+r = [1,2,3,4,5,6,7]
+def filter_1(n):
+    if n > 5:
+        return True
+    else:
+        return False
+
+numbers = filter(filter_1 , r)
+print(list(numbers))
